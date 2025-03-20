1f14348229e9cf840ad245c417e94da071471cd0
Manuel Melendez <142455714+mmelndezlujn@users.noreply.github.com>
2025-03-20 08:32:56 -0700

    Update s005.ps1

--- s005.ps1
+++ s005.ps1
@@ -2,7 +2,7 @@
 Connect-AzAccount -Identity | Out-Null
 
 # Obtain the access token for Azure Management API
-$token = (Get-AzAccessToken -ResourceUrl "https://vault.azure.net").Token
+$token = (Get-AzAccessToken -ResourceUrl "https://management.azure.com").Token
 
 # Output the token (for debugging purposes)
 Write-Output $token
